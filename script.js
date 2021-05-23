17bd3a94eb677c90e5e7528c0209cf1c017e3fc1
faiza203 <fiazfaiza981@gmail.com>
2021-05-23 15:29:23 +0500

    Completed Project

--- script.js
+++ script.js
@@ -1,49 +1,162 @@
-const loader = document.querySelector(".loader");
-let dataArr = [];
 
-const fetchData = async () => {
-  await fetch(
-    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      dataArr = data.data;
-      createElements(dataArr);
-      loader.style.visibility = "hidden";
-    });
-};
+      var width = 800,
+      height = 400,
+      barWidth = width / 275;
 
-const createElements = (dataA) => {
-  d3.select(".data")
-    .data(dataA)
-    .enter()
-    .append("p")
-    .text((d) => d[1]);
-  // const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
-  console.log(dataArr);
-  const w = 900;
-  const h = 460;
+    var tooltip = d3
+      .select(".visHolder")
+      .append("div")
+      .attr("id", "tooltip")
+      .style("opacity", 0);
 
-  const svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+    var overlay = d3
+      .select(".visHolder")
+      .append("div")
+      .attr("class", "overlay")
+      .style("opacity", 0);
 
-  svg
-    .selectAll("rect")
-    .data(dataArr)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => (i * dataArr.length * 10) / w)
-    .attr("data-date" , d => d[0])
-    .attr("transform", "translate(60, 0)")
-    .attr("y", (d, i) => 400 - (i + d[1]) / 45.8)
-    .attr("fill", "rgb(51, 173, 255)")
-    .attr("width", 2.909090909090909)
-    .attr("height", (d, i) => (i + d[1]) / 45.8);
-};
+    var svgContainer = d3
+      .select(".visHolder")
+      .append("svg")
+      .attr("width", width + 100)
+      .attr("height", height + 60);
 
-fetchData();
+    d3.json(
+      "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json",
+      function (e, data) {
+        svgContainer
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("x", -200)
+          .attr("y", 80)
+          .text("Gross Domestic Product");
+
+        svgContainer
+          .append("text")
+          .attr("x", width / 2 + 120)
+          .attr("y", height + 50)
+          .text(
+            "More Information: http://www.bea.gov/national/pdf/nipaguid.pdf"
+          )
+          .attr("class", "info");
+
+        var years = data.data.map(function (item) {
+          var quarter;
+          var temp = item[0].substring(5, 7);
+
+          if (temp === "01") {
+            quarter = "Q1";
+          } else if (temp === "04") {
+            quarter = "Q2";
+          } else if (temp === "07") {
+            quarter = "Q3";
+          } else if (temp === "10") {
+            quarter = "Q4";
+          }
+
+          return item[0].substring(0, 4) + " " + quarter;
+        });
+
+        var yearsDate = data.data.map(function (item) {
+          return new Date(item[0]);
+        });
+
+        var xMax = new Date(d3.max(yearsDate));
+        xMax.setMonth(xMax.getMonth() + 3);
+        var xScale = d3
+          .scaleTime()
+          .domain([d3.min(yearsDate), xMax])
+          .range([0, width]);
+
+        var xAxis = d3.axisBottom().scale(xScale);
+
+        svgContainer
+          .append("g")
+          .call(xAxis)
+          .attr("id", "x-axis")
+          .attr("transform", "translate(60, 400)");
+
+        var GDP = data.data.map(function (item) {
+          return item[1];
+        });
+
+        var scaledGDP = [];
+
+        var gdpMax = d3.max(GDP);
+
+        var linearScale = d3
+          .scaleLinear()
+          .domain([0, gdpMax])
+          .range([0, height]);
+
+        scaledGDP = GDP.map(function (item) {
+          return linearScale(item);
+        });
+
+        var yAxisScale = d3
+          .scaleLinear()
+          .domain([0, gdpMax])
+          .range([height, 0]);
+
+        var yAxis = d3.axisLeft(yAxisScale);
+
+        svgContainer
+          .append("g")
+          .call(yAxis)
+          .attr("id", "y-axis")
+          .attr("transform", "translate(60, 0)");
+
+        d3.select("svg")
+          .selectAll("rect")
+          .data(scaledGDP)
+          .enter()
+          .append("rect")
+          .attr("data-date", function (d, i) {
+            return data.data[i][0];
+          })
+          .attr("data-gdp", function (d, i) {
+            return data.data[i][1];
+          })
+          .attr("class", "bar")
+          .attr("x", function (d, i) {
+            return xScale(yearsDate[i]);
+          })
+          .attr("y", function (d) {
+            return height - d;
+          })
+          .attr("width", barWidth)
+          .attr("height", function (d) {
+            return d;
+          })
+          .style("fill", "#33adff")
+          .attr("transform", "translate(60, 0)")
+          .on("mouseover", function (d, i) {
+            overlay
+              .transition()
+              .duration(0)
+              .style("height", d + "px")
+              .style("width", barWidth + "px")
+              .style("opacity", 0.9)
+              .style("left", i * barWidth + 0 + "px")
+              .style("top", height - d + "px")
+              .style("transform", "translateX(60px)");
+            tooltip.transition().duration(200).style("opacity", 0.9);
+            tooltip
+              .html(
+                years[i] +
+                  "<br>" +
+                  "$" +
+                  GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +
+                  " Billion"
+              )
+              .attr("data-date", data.data[i][0])
+              .style("left", i * barWidth + 30 + "px")
+              .style("top", height - 100 + "px")
+              .style("transform", "translateX(60px)");
+          })
+          .on("mouseout", function () {
+            tooltip.transition().duration(200).style("opacity", 0);
+            overlay.transition().duration(200).style("opacity", 0);
+          });
+      }
+    );
